a6206d1fb9bf706944065b95d8c988e0a6dcbda4
agulevski <gusguleal@student.gu.se>
2016-04-20 14:53:11 +0200

    fixed

--- Safari/src/com/rastsafari/model/CustomerCategoryList.java
+++ Safari/src/com/rastsafari/model/CustomerCategoryList.java
@@ -1,5 +1,42 @@
 package com.rastsafari.model;
 
-public class CustomerCategoryList {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class CustomerCategoryList {
+	private ObservableList<CustomerCategory> categories;
+
+	public CustomerCategoryList(){
+		categories = FXCollections.observableArrayList();
+		getCategoriesFromDatabase();
+	}
+	
+	private void getCategoriesFromDatabase(){
+		SafariDatabase sd = new SafariDatabase();
+		Connection c = sd.createConnection();
+		double pi;
+		try{
+			Statement statement = c.createStatement();
+			String sql = "SELECT * FROM customerCategory;";
+			
+			ResultSet rs = statement.executeQuery(sql);
+			
+			while(rs.next()){
+				categories.add(new CustomerCategory(
+						rs.getInt("id"),
+						rs.getString("categoryName"),
+						pi = rs.getInt("priceIndex")));
+			}
+			rs.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	public ObservableList<CustomerCategory> getCustomerCategoryList(){
+		return categories;
+	}
 }
