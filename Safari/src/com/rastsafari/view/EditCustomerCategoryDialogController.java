243eb304ecc96efaba700fdb5d14031ced3085b9
NurNauti <Nuri@dhcp-079177.eduroam.chalmers.se>
2016-04-21 12:24:58 +0200

    rör ej.

--- Safari/src/com/rastsafari/view/EditCustomerCategoryDialogController.java
+++ Safari/src/com/rastsafari/view/EditCustomerCategoryDialogController.java
@@ -2,7 +2,13 @@ package com.rastsafari.view;
 
 import java.awt.TextField;
 
+import com.rastsafari.model.CustomerCategory;
+import com.rastsafari.model.CustomerMaintenance;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 public class EditCustomerCategoryDialogController {
 	@FXML
@@ -10,10 +16,37 @@ public class EditCustomerCategoryDialogController {
 	@FXML
 	private TextField priceIndexField;
 	
-	private Stage dialogStage;
+	private Stage categoryStage;
 	private CustomerCategory category;
-	private SafariMaintenace maintenance = new SafariMaintenance();
+	private CustomerMaintenance maintenance = new CustomerMaintenance();
 	private boolean okClicked = false;
-	private ObservableList<>
+	
+	@FXML
+	private void initialize(){} 
+	
+	public void setCategoryStage(Stage categoryStage){
+		this.categoryStage = categoryStage;
+	}
+	
+	public void setCategory(CustomerCategory category){
+		this.category = category; 
+		
+		categoryNameField.setText(category.getCategoryName());
+		priceIndexField.setText(String.valueOf(category.getPriceIndex()));
+			
+	} 
+	
+
+	@FXML
+	private void handleOK(){
+		
+	}
+	
+	/*private boolean isInputValid(){
+		String errorMessage = "";
+		if(categoryNameField.get)
+	}**/
+	
+	
 
 }
