7e86c68767431f639c388c83175afabcc0a7e0c9
agulevski <gusguleal@student.gu.se>
2016-04-21 11:53:37 +0200

    fixed

--- Safari/src/com/rastsafari/view/EditCustomerCategoryDialogController.java
+++ Safari/src/com/rastsafari/view/EditCustomerCategoryDialogController.java
@@ -1,5 +1,19 @@
 package com.rastsafari.view;
 
+import java.awt.TextField;
+
+import javafx.fxml.FXML;
+
 public class EditCustomerCategoryDialogController {
+	@FXML
+	private TextField categoryNameField;
+	@FXML
+	private TextField priceIndexField;
+	
+	private Stage dialogStage;
+	private CustomerCategory category;
+	private SafariMaintenace maintenance = new SafariMaintenance();
+	private boolean okClicked = false;
+	private ObservableList<>
 
 }
