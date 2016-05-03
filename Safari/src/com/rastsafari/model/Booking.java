3ffbd6a1412106b857efd359c915ebb484202def
agulevski <gusguleal@student.gu.se>
2016-05-03 14:02:38 +0200

    fixade booking model

--- Safari/src/com/rastsafari/model/Booking.java
+++ Safari/src/com/rastsafari/model/Booking.java
@@ -8,30 +8,19 @@ import javafx.beans.property.StringProperty;
 public class Booking {
 	
 	private IntegerProperty id;
-	private	IntegerProperty bookingNr;
-	private IntegerProperty paymentNr;
-	private IntegerProperty invoiceNr;
-	private IntegerProperty ocrNr;
-	private IntegerProperty safariId;
-	private StringProperty date;
-	private StringProperty hour;
-	private StringProperty endHour;
+	private Customer customer;
+	private Safari safari;
+
 	
 	public Booking(){
-		this(0,0,0,0,0,0,null,null,null);
+		this(0,null,null);
 	}
 	
-public Booking(int id, int bookingNr, int paymentNr, int invoiceNr,int ocrNr, int safariId, String date,
-	String hour, String endHour){
+	public Booking(int id,Customer customer, Safari safari){
 		this.id= 			new SimpleIntegerProperty(id);
-		this.bookingNr= 	new SimpleIntegerProperty(bookingNr);
-		this.paymentNr= 	new SimpleIntegerProperty(paymentNr); 
-		this.invoiceNr= 	new SimpleIntegerProperty(invoiceNr);
-		this.ocrNr=			new SimpleIntegerProperty(ocrNr);
-		this.safariId= 		new SimpleIntegerProperty(safariId);
-		this.date= 			new SimpleStringProperty(date);
-		this.hour= 			new SimpleStringProperty(hour);
-		this.endHour= 		new SimpleStringProperty(endHour);
+		this.customer= 		customer;
+		this.safari= 		safari;
+
 	}
 	
 	public int getId(){
@@ -45,101 +34,11 @@ public Booking(int id, int bookingNr, int paymentNr, int invoiceNr,int ocrNr, in
 	public IntegerProperty idProperty(){
 		return id;
 	}
-	
-	public int getBookingNr(){
-		return bookingNr.get();
+	public Customer getCustomer(){
+		return customer;
 	}
 	
-	public void setBookingNr(int bookingNr){
-		this.bookingNr.set(bookingNr);
+	public Safari getSafari(){
+		return safari;
 	}
-	
-	public IntegerProperty bookingNrProperty(){
-		return bookingNr;
-	}
-	
-	public int getPaymentNr(){
-		return paymentNr.get();
-	}
-	
-	public void setPaymentNr(int paymentNr){
-		this.paymentNr.set(paymentNr);
-	}
-	
-	public IntegerProperty paymentNrProperty(){
-		return paymentNr;
-	}
-	
-	public int getInvoiceNr(){
-		return invoiceNr.get();
-	}
-	
-	public void setInvoiceNr(int invoiceNr){
-		this.invoiceNr.set(invoiceNr);
-	}
-	
-	public IntegerProperty invoiceNrProperty(){
-		return invoiceNr;
-	}
-	
-	public int getOcrNr(){
-		return ocrNr.get();
-	}
-	
-	public void setOcrNr(int ocrNr){
-		this.ocrNr.set(ocrNr);
-	}
-	
-	public IntegerProperty ocrNrProperty(){
-		return ocrNr;
-	}
-	
-	public int getSafarId(){
-		return safariId.get();
-	}
-	
-	public void setSafariId(int safariId){
-		this.safariId.set(safariId);
-	}
-	
-	public IntegerProperty safariIdPropert(){
-		return safariId;
-	}
-	
-	public String getDate(){
-		return date.get();
-	}
-	
-	public void setDate(String date){
-		this.date.set(date);
-	}
-	
-	public StringProperty dateProperty(){
-		return date;
-	}
-	
-	public String getHour(){
-		return hour.get();
-	}
-	
-	public void setHour(String hour){
-		this.hour.set(hour);
-	}
-	
-	public StringProperty hourProperty(){
-		return hour;
-	}
-	
-	public String getEndHour(){
-		return endHour.get();
-	}
-	
-	public void setEndHour(String endHour){
-		this.endHour.set(endHour);
-	}
-	
-	public StringProperty endHourProperty(){
-		return endHour;
-	}
-	
 }
